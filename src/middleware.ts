4da068ed8a456404216cd79b2a7d18def8da368e
Varun5711 <varunhotani@gmail.com>
2025-07-29 23:33:47 +0530

    commiting

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,27 @@
+import { clerkMiddleware } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
-  // Example middleware logic
+const interviewerRoutes = ["/dashboard(.*)", "/recordings(.*)" , "/schedule(*)"];
+
+export default clerkMiddleware(async (auth, req) => {
+  const { userId, sessionClaims } = await auth();
+
+  const url = req.nextUrl;
+  const path = url.pathname;
+
+  if (!userId) {
+    return NextResponse.redirect(new URL("/sign-in", req.url));
+  }
+
+  const role = (sessionClaims?.publicMetadata as { role?: string })?.role;
+
+  if (interviewerRoutes.some(route => path.startsWith(route)) && role !== "interviewer") {
+    return NextResponse.redirect(new URL("/unauthorized", req.url));
+  }
+
+
   return NextResponse.next();
-}
+});
 
 export const config = {
   matcher: [
